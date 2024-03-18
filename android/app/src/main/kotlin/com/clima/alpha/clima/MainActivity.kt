d00fef013c77c66cff14772c81b8096a3a13224a
harshfreikin <harshtiwari8880@gmail.com>
2024-03-18 17:39:39 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/clima/alpha/clima/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.clima.alpha.clima
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utilities/constant.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
+
+const apiKey = '28573d08277b26d202750799e10af022';
+const openWeatherUrl='https://api.openweathermap.org/data/2.5/weather?';
